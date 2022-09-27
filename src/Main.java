74aa2245e5fbbbb67aba7ebc6655edd5b73de215
UgurKADIOGLU <ugrkadioglu@gmail.com>
2022-09-27 15:45:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        topla();
+        mesajVer();
+    }
+
+    public static void topla() {
+        int sayi1 = 5;
+        int sayi2 = 10;
+        int toplam = 0;
+        toplam = sayi1 + sayi2;
+        System.out.println(toplam);
+    }
+
+    public static void mesajVer() {
+        System.out.println("Mesaj ver...");
+    }
+}
